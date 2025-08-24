43903305434a1cef55e122ef508e362df992ce79
junchh <cjunho9868@gmail.com>
2025-08-25 00:17:42 +0700

    chore: fix formatting

--- init.lua
+++ init.lua
@@ -1,18 +1,17 @@
 --
---				||========================================||
---				||																				||
---				||			    Neovim Configuration   				||
---				||								  v1.0						  		||
---        ||                                        ||
---				||						 @author junchh             ||
---        ||                                        ||
---				||           Based on kickstart           ||
---				||       Heavily inspired by LazyVim			||
---        ||                                        ||
---        ||                                        ||
---				||========================================||
+--              ||========================================||
+--              ||                                        ||
+--              ||            Neovim Configuration        ||
+--              ||                     v1.0               ||
+--              ||                                        ||
+--              ||              @author junchh            ||
+--              ||                                        ||
+--              ||           Based on kickstart           ||
+--              ||       Heavily inspired by LazyVim      ||
+--              ||                                        ||
+--              ||                                        ||
+--              ||========================================||
 --
-
 require("config.options")
 require("config.keymaps")
 require("config.utils")
